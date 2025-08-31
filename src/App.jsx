9cb1d8483a13cc262b15b37685cbeedd71bd0f7a
Dendup Tshering <dendup091@gmail.com>
2025-09-01 01:41:31 +0600

    Added the tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Tailwind from "./Tailwind";
 function App() {
   return (
     <>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      <Tailwind></Tailwind>
     </>
   );
 }
---
+++ src/Tailwind.jsx
@@ -0,0 +1,5 @@
+function Tailwind() {
+  return <h1 className="text-3xl text-bold underline">Hello world!</h1>;
+}
+
+export default Tailwind;
