fa2f8242f68634491b81f24d57826c319199bb08
dendup1234 <dendup091@gmail.com>
2025-09-03 23:58:02 +0600

    Learned grid and flex layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Student from "./Student";
+import Container from "./Container";
 function App() {
   return (
     <>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      <div className="container mx-auto p-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <Student name="Dendup" age={18}></Student>
+          <Student name="Dendup" age={18}></Student>
+          <Student name="Dendup" age={18}></Student>
+          <Student name="Dendup" age={18}></Student>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Container.jsx
@@ -0,0 +1,10 @@
+function Container() {
+  return (
+    <div className="container mx-auto px-4">
+      <div className="bg-red-600 max-w-[200px]">Red</div>
+      <div className="bg-blue-700">Blue</div>
+    </div>
+  );
+}
+
+export default Container;
